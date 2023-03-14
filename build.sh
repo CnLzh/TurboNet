e4c62c74a351fdfbf275d033ba36bf391ace0220
CnLzh <cnlzh1516@outlook.com>
2023-03-14 22:29:58 +0800

    update build.sh

--- build.sh
+++ build.sh
@@ -1,25 +1,25 @@
-# !/bin/bash
+#!/bin/bash
 
 set -e
 
-SOURCE_DIR=`pwd`
+SOURCE_DIR=$(pwd | tr -d '\0')
 
 # 创建build目录
-if [ ! -d `pwd`/build ]; then
-	mkdir `pwd`/build
+if [ ! -d "${SOURCE_DIR}/build" ]; then
+	mkdir "${SOURCE_DIR}/build"
 fi
 
 # 创建lib目录
-if [ ! -d `pwd`/lib ]; then
-	mkdir `pwd`/lib
+if [ ! -d "${SOURCE_DIR}/lib" ]; then
+	mkdir "${SOURCE_DIR}/lib"
 fi
 
 # 创建include目录
-if [ ! -d `pwd`/include ]; then
-	mkdir `pwd`/include
+if [ ! -d "${SOURCE_DIR}/include" ]; then
+	mkdir "${SOURCE_DIR}/include"
 fi
 
-rm -rf ${SOURCE_DIR}/build/*
+rm -rf "${SOURCE_DIR}/build/*"
 
-cd ${SOURCE_DIR}/build && cmake .. && make install
+cd "${SOURCE_DIR}/build" && cmake .. && make install
 
