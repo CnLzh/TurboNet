cca74807b4a68287bd0e895971b1b14c38976d44
CnLzh <cnlzh1516@outlook.com>
2023-03-17 00:17:42 +0800

    Rewrite timestamp using chrono.

--- src/base/timestamp.cpp
+++ src/base/timestamp.cpp
@@ -24,10 +24,8 @@ std::string Timestamp::ToString() const {
 
 std::string Timestamp::ToFormatString(bool show_microseconds) const {
   tb_s8 buf[64] = {0};
-
   auto seconds = std::chrono::duration_cast<std::chrono::seconds>(time_since_epoch_).count();
   auto local_time = std::localtime(&seconds);
-
   if (show_microseconds) {
 	auto milliseconds = std::chrono::duration_cast<std::chrono::milliseconds>(time_since_epoch_).count();
 	auto microseconds = std::chrono::duration_cast<std::chrono::microseconds>(time_since_epoch_).count();
@@ -40,7 +38,6 @@ std::string Timestamp::ToFormatString(bool show_microseconds) const {
 			 local_time->tm_year + 1900, local_time->tm_mon + 1, local_time->tm_mday,
 			 local_time->tm_hour, local_time->tm_min, local_time->tm_sec);
   }
-
   return buf;
 }
 
