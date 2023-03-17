e7916986f04f15f08faa51a9fc0347915404261a
lzh <liu2308078345@outlook.com>
2023-03-17 16:27:26 +0800

    update timestamp

--- src/base/timestamp.cpp
+++ src/base/timestamp.cpp
@@ -18,7 +18,7 @@ std::string Timestamp::ToString() const {
   tb_s8 buf[32] = {0};
   auto seconds = std::chrono::duration_cast<std::chrono::seconds>(time_since_epoch_).count();
   auto microseconds = std::chrono::duration_cast<std::chrono::microseconds>(time_since_epoch_).count();
-  snprintf(buf, sizeof(buf) - 1, "%ld.%06ld", seconds, microseconds);
+  snprintf(buf, sizeof(buf) - 1, "%ld.%06ld", seconds, microseconds % kMicroSecondsPerSecond);
   return buf;
 }
 
@@ -27,12 +27,10 @@ std::string Timestamp::ToFormatString(bool show_microseconds) const {
   auto seconds = std::chrono::duration_cast<std::chrono::seconds>(time_since_epoch_).count();
   auto local_time = std::localtime(&seconds);
   if (show_microseconds) {
-	auto milliseconds = std::chrono::duration_cast<std::chrono::milliseconds>(time_since_epoch_).count();
 	auto microseconds = std::chrono::duration_cast<std::chrono::microseconds>(time_since_epoch_).count();
-	snprintf(buf, sizeof(buf), "%4d-%02d-%02d %02d:%02d:%02d.%03ld.%06ld",
+	snprintf(buf, sizeof(buf), "%4d-%02d-%02d %02d:%02d:%02d.%06ld",
 			 local_time->tm_year + 1900, local_time->tm_mon + 1, local_time->tm_mday,
-			 local_time->tm_hour, local_time->tm_min, local_time->tm_sec,
-			 milliseconds % 1000, microseconds % 1000);
+			 local_time->tm_hour, local_time->tm_min, local_time->tm_sec, microseconds % kMicroSecondsPerSecond);
   } else {
 	snprintf(buf, sizeof(buf), "%4d-%02d-%02d %02d:%02d:%02d",
 			 local_time->tm_year + 1900, local_time->tm_mon + 1, local_time->tm_mday,
