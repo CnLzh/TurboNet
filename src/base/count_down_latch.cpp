dfc77ba2e03d3ecf1d9b42b317eb59f9e1c17634
CnLzh <cnlzh1516@outlook.com>
2023-03-18 22:40:47 +0800

    update count_down_latch

---
+++ src/base/count_down_latch.cpp
@@ -0,0 +1,30 @@
+#include "count_down_latch.h"
+
+namespace turbo {
+
+CountDownLatch::CountDownLatch(const tb_s32 &count)
+	: count_(count),
+	  mutex_(),
+	  cond_(mutex_) {
+
+}
+
+void CountDownLatch::Wait() {
+  MutexLockGuard lock(mutex_);
+  while (count_ > 0)
+	cond_.Wait();
+}
+
+void CountDownLatch::CountDown() {
+  MutexLockGuard lock(mutex_);
+  --count_;
+  if (count_ == 0)
+	cond_.NotifyAll();
+}
+
+tb_s32 CountDownLatch::GetCount() const {
+  MutexLockGuard lock(mutex_);
+  return count_;
+}
+
+} // turbo
---
+++ src/base/count_down_latch.h
@@ -0,0 +1,32 @@
+// Copyright (C) 2023
+//
+// File:     count_down_latch.h
+// Brief:    CountDownLatch to provide inter-thread synchronization.
+// Author:   CnLzh
+
+#ifndef TURBONET_SRC_BASE_COUNT_DOWN_LATCH_H_
+#define TURBONET_SRC_BASE_COUNT_DOWN_LATCH_H_
+
+#include "public_define.h"
+#include "condition.h"
+
+namespace turbo {
+
+class CountDownLatch {
+ public:
+  explicit CountDownLatch(const tb_s32 &count);
+  void Wait();
+  void CountDown();
+  tb_s32 GetCount() const;
+
+ private:
+  mutable MutexLock mutex_;
+  Condition cond_;
+  tb_s32 count_;
+  DISALLOW_COPY_AND_ASSIGN(CountDownLatch)
+
+};
+
+} // turbo
+
+#endif //TURBONET_SRC_BASE_COUNT_DOWN_LATCH_H_
